f66c752f67f96c6198ab19684c2eac77a9c8aa68
luis76252116 <43302609+luis76252116@users.noreply.github.com>
2018-09-15 16:25:09 -0400

    Add files via upload

---
+++ ENCENDER_UN_LED_CON_DOS_PULSADORES.ino
@@ -0,0 +1,26 @@
+int ldr=A4;
+int salida=3;
+int rele=8;
+int ldrValue=0;
+void setup() {
+  pinMode (ldr,INPUT);
+  pinMode (salida,OUTPUT);
+  pinMode (rele,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  ldrValue = analogRead(ldr);
+  Serial.println(ldrValue,DEC);
+ if(analogRead(ldr)<=200)
+ {
+  digitalWrite(salida,HIGH);
+  digitalWrite(rele,HIGH);
+ }
+ else
+ {
+  digitalWrite(salida,LOW);
+  digitalWrite(rele,LOW);
+ }
+}
+
